92c7f58e59cfc446e7ab2cc6103bbcee5a2b4bf8
olusegun <olusegunbamgbelu755@gmail.com>
2024-08-19 19:39:41 +0100

    add mobile nav bar

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 let hamburgerIcon = document.querySelector('.hamburger-icon')
 let hamburgerImage = document.querySelector('.hamburger-image')
+let closeIcon = document.querySelector('.close-icon');
+let mobileNav = document.querySelector('.mobile-nav');
 
-hamburgerIcon.addEventListener("click", function() {
-    hamburgerImage.setAttribute("src", "./images/icon-close.svg")
-})
+hamburgerIcon.addEventListener("click", showNavBar);
+
+function showNavBar() {
+    hamburgerIcon.classList.add("hidden");
+    closeIcon.classList.remove("hidden");
+    mobileNav.classList.add("flex");
+    mobileNav.classList.remove("hidden");
+}
+
+closeIcon.addEventListener('click', hideNavBar)
+
+function hideNavBar() {
+
+    closeIcon.classList.add("hidden")
+    hamburgerIcon.classList.remove("hidden");
+
+    mobileNav.classList.remove("flex");
+    mobileNav.classList.add("hidden");
+}
